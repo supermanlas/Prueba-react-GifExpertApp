124368ef52cf0107ef89d421147875bea2884183
Esteban Alvarado <esteban_5.4@hotmail.com>
2021-07-12 18:36:23 -0400

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=6clfOGQ9ghL0x1oD9F4qnOrnEkybyNkk`;
+    const respuesta = await fetch(url);
+    const { data } = await respuesta.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
